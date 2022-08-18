8e404ae57bbd86ac5fc2187b920328ed5e587c80
alex-obada <alex2539gg@gmail.com>
2022-08-18 20:20:28 +0300

    feat: trimitere de mesaje
    index.js trimite un mesaj la user-ul specificat de id-ul din config.json
    si se inchide

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token, id } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const message = "mesaj prietene";
+
+const logError = () => {
+    console.error;
+    process.exit(1);
+};
+
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", async () => {
+    console.log("Running...");
+
+    // const { commandName } = interaction;
+
+    const user = await client.users.fetch(id).catch(logError);
+    await user
+        .send(message)
+        .catch(logError)
+        .then(() => process.exit(0));
 });
 
-console.log(token);
 // Login to Discord with your client's token
 client.login(token);
